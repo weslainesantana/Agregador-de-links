c3eb6400fd3bfb761b9d68ba58287e84b3f1ef60
weslainesantana <weslaine2029@gmail.com>
2024-08-26 14:20:03 -0300

    first commit

---
+++ .vs.code/script.js
@@ -0,0 +1,32 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+//pegar a tag img
+const img = document.querySelector("#profile img")
+
+//substituir a imagem
+if (html.classList.contains('light')) {
+    //se tiver light mode, adicionar imagem light
+    img.setAttribute("src", "assets/avatar-light.png")
+} else {
+    //se tiver sem light mode, manter a imagem normal
+
+    img.setAttribute("src", "assets/avatar.png")
+}
+}
+
+
+
+
+
+
+
+//if (html.classList.contains('light')){
+// html.classList.remove('light')
+//} else {
+// html.classList.add('light')
+//}
+
+
+
